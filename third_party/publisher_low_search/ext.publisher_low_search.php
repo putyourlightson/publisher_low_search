2515e2828a6a1a4b8f53fcdd5925eab759ad2363
Brian Litzinger <litzinger@gmail.com>
2013-07-30 07:23:50 -0500

    Cleaned up conditional

--- third_party/publisher_low_search/ext.publisher_low_search.php
+++ third_party/publisher_low_search/ext.publisher_low_search.php
@@ -92,14 +92,7 @@ class Publisher_low_search_ext {
 
         foreach ($fields as $k => $field_id)
         {
-            if ($field_id == 0)
-            {
-                $field_names[] = 't.title';
-            }
-            else
-            {
-                $field_names[] = 'd.field_id_'.$field_id;
-            }
+            $field_names[] = ($field_id == 0) ? 't.title' : 'd.field_id_'.$field_id;
         }
 
         // --------------------------------------
